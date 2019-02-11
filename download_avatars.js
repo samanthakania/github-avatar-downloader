b222474c0baa77a751e7b36a3c669a2f30739269
Samantha Kania <samantha.kania12@gmail.com>
2019-02-11 22:37:22 +0000

    avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,13 @@
 var request = require('request');
-var secrets = require('.secrets.js')
+var secrets = require('./secrets.js')
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
+      Authorization: secrets.GITHUB_TOKEN
     }
   };
 
@@ -18,4 +19,12 @@ function getRepoContributors(repoOwner, repoName, cb) {
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  var info = JSON.parse(result);
+  info.forEach(function(item){
+    console.log(item.avatar_url)
+  });
+
+function downloadImageByURL(url, filePath) {
+  // ...
+}
 });
